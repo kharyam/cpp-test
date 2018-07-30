4d408c49d444676dde8e971492becee866a9fbcc
Khary Mendez <kharyam@gmail.com>
2018-07-30 18:19:24 -0500

    Code update

--- test.cpp
+++ test.cpp
@@ -1,18 +1,16 @@
 // Simple C++ program to display "Hello World"
- 
-// Header file for input output functions
-#include <iostream> 
-#include <unistd.h>
 
-using namespace std;
+// Header file for input output functions
+#include <iostream>
+#include <unistd.h>
  
 int main()
 {
-    for (int i = 0 ; i < 5 ; i++)
-    {
-        cout << "Hello World " << i << "\n";
-        cout.flush();
-        usleep(1000000);
-    }    
-    return 0;
+  for (int i = 0 ; i < 5 ; i++)
+  {
+    std::cout << "Hello World!! " << i << "\n";
+    std::cout.flush();
+    usleep(1000000);
+  }
+  return 0;
 }
